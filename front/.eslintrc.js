7aae2c02d8fe70c7720a15bd8810580f6e1db1de
hyeon924 <hyeonn4713@gmail.com>
2025-07-29 14:40:56 +0900

    eslint 설정 CJS 형식으로 수정

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,10 +1,9 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    // _로 시작하는 unused 변수는 허용 (e.g. (_, idx) => ...)
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-
     'react-hooks/exhaustive-deps': 'warn',
   },
 };
